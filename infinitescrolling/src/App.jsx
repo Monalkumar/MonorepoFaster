0bfb4f58e806a4ffa55f1c1bcea3ae19ef373bcb
MonalKumar <monalshiv123@gmail.com>
2025-10-04 16:01:12 +0530

    initial commit

--- infinitescrolling/src/App.jsx
+++ infinitescrolling/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState } from 'react'
 
 import './App.css'
 
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <div>
-        Hello
+        <h1>Hello</h1>
       </div>
       
     </>
