5ca2c4d55e1e741a77118119ada7a44ca7f14c01
MonalKumar <monalshiv123@gmail.com>
2025-10-10 15:55:59 +0530

    bhakti marg

--- reactwithredux/src/features/users/userSlice.js
+++ reactwithredux/src/features/users/userSlice.js
@@ -1,15 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const savedCount = JSON.parse(localStorage.getItem("count") || "0");
+
 const counterSlice = createSlice({
   name: "counter",
-  initialState: { value: 0 },
+  initialState: { value: savedCount },
   reducers: {
     increamment: (state) => {
       state.value = state.value + 1;
+      localStorage.setItem("count", JSON.stringify(state.value));
     },
   },
 });
 
-export const { increamment, decreament, increamentByNums } =
-  counterSlice.actions;
+export const { increamment } = counterSlice.actions;
 export default counterSlice.reducer;
