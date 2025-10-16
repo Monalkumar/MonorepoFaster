a14da5e9aa9fe7a22d5e3642644db977cb6918fa
MonalKumar <monalshiv123@gmail.com>
2025-10-16 23:28:46 +0530

    dynamic datas

--- FriendshipApp/backend/src/app.js
+++ FriendshipApp/backend/src/app.js
@@ -3,23 +3,46 @@ const connectDB=require("./config/database")
 const app = express();
 const User = require("./models/user")
 
-app.post("/signup",(req,res)=>{
+
+app.use(express.json())
+
+// app.post("/signup",(req,res)=>{
     
     // creating new instances of the userModel
-    const user = new User({
-        name:"johny",
-        email:"jhony@gmail.com",
-        passWord:"12345",
-        age:97,
-        gender:"maleandfemale"
-    })
-    try{
-    user.save();
-    res.send("workig perfect")
-    }
-    catch(err){
-        res.status(401).send("please reslolve err",err.message)
-    }
+    // Manual way to send data to database
+//     const user = new User({
+        // name:"johnyy",
+//         email:"jhony@gmail.com",
+//         passWord:"12345",
+//         age:97,
+//         gender:"maleandfemale"
+//     })
+    // try{
+    // user.save();
+    // res.send("workig perfect")
+    // }
+    // catch(err){
+    //     res.status(401).send("please reslolve err",err.message)
+    // }
+    
+// })
+
+app.post("/signup",async(req,res)=>{
+   try {
+    console.log(req.body);
+     // Create a new user from request body
+    const user = new User(req.body);
+    // Save to MongoDB
+    await user.save();
+    // Send success response
+    res.status(201).json({
+      message: "User registered successfully",
+      user,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
     
 })
 
@@ -30,6 +53,6 @@ app.listen(5000,()=>{
     console.log("backend is working fast and smooth")
 })
 }).catch((err)=>{
-    consle.log('error pleas  resolve')
+    console.log('error pleas  resolve')
 })
 
