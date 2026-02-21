5d15c0610d115a7d98bf7a5d827c1c0d140ccc42
MonalKumar <monalshiv123@gmail.com>
2026-02-21 23:03:35 +0530

    todos app practice

--- todolist/src/components/TodosApp.jsx
+++ todolist/src/components/TodosApp.jsx
@@ -1,9 +1,55 @@
-import React from "react";
+import React,{useState} from "react";
 
 const TodosApp =()=>{
+    const [todo,setTodo] = useState("");
+    const [todos,setTodos] = useState([]);
+    const[editId,setEditId] = useState( null)
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+
+        if(editId){
+            const updatedTodo = todos.map((t)=>t.id ===editId?{id:t.id,todo}:t);
+            setTodos(updatedTodo);
+            setEditId(null)
+            setTodo("")
+            return
+        }
+        if(todo !== " " ){
+            setTodos([{id:`${todo.id}-${Date.now()}`,todo}, ...todos])
+        }
+        setTodo("")
+    }
+
+    const handleDelte = (id)=>{
+        const delTodos = todos.filter((tdl)=>tdl.id !==id);
+        setTodos(delTodos)
+    }
+    const handleEdit =(id)=>{
+        const editTodos = todos.find((i)=>i.id ===id);
+        setTodo(editTodos.todo)
+        setEditId(id)
+    }
     return(
-        <div>Hello</div>
-    )
+        <div>
+        <form onSubmit={handleSubmit}> 
+        <input  type="text" value={todo} placeholder="search here ...." onChange={(e)=>setTodo(e.target.value)}/>
+        <button type="submit">Add Todos</button>
+        </form>
+        <div>
+        <ul>
+        
+        {todos.map((t)=>(
+            <div><span>{t.todo}</span>
+            <button type ="button" onClick={()=>handleEdit(t.id)}>Edit</button>
+            <button type="button" onClick={()=>handleDelte(t.id)}>Delet</button>
+            </div>
+        ))}
+        
+        </ul>
+        </div>
+        </div>
+    ) 
 }
 
 export default TodosApp;
