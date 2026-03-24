068910e11ca6d8b2a104f72d6f119a8bea060d7b
MonalKumar <monalshiv123@gmail.com>
2026-03-24 23:04:14 +0530

    inifinite scrolling practice

--- infinitescrolling/src/components/InfiniteScrolls.jsx
+++ infinitescrolling/src/components/InfiniteScrolls.jsx
@@ -1,76 +1,80 @@
-import React, { useState,useEffect } from "react";
+import React, {useState,useEffect} from "react";
+import { useCallback } from "react";
 
-const InfiniteScrolls = () => {
-    const [products,setProducts] = useState([]);
-    const[page,setPage] = useState(0);
+const InfiniteScrolls = ()=>{
+    const[products,setProducts] = useState([]);
+    const [page,setPage] = useState(0);
     const[loading,setLoading] = useState(false);
-    const[hasMore,setHasMore] = useState(true)
-    const[error,setError] = useState(null)
-
-    const fetchAPis = async ()=>{
-     
-        const LIMIT = 10;
+    const[error,setError] = useState(null);
+    const[hasMore,sethasMore] = useState(true)
+    const fetchApis = async()=>{
+        
         try{
-            setError(null);
-            setLoading(true)
-              const skip = page*LIMIT
+            const LIMIT= 10;
+            const skip = page*LIMIT
+            setLoading(true);
+            setError(null)
         const response = await fetch(`https://dummyjson.com/products?limit=${LIMIT}&skip=${skip}`);
         if(!response.ok){
-            throw new Error(`https error ${response.status}`)
+            throw new Error(`Https status ${response.status}`)
         }
         const result = await response.json();
-        setProducts((prev)=>[...prev,...result.products])
-         if(result.products.length===0) return setHasMore(false)
+        setProducts((prev)=>[...prev,...result.products]);
+        if(result.products.length===0) return sethasMore(false);
+        if(result.products.length < LIMIT){
+        sethasMore(false)
+        }
         }
         catch(error){
-            setError(`error message ${error.message}`)
+            setError(`error:${error.message}`)
         }
         finally{
             setLoading(false)
         }
-        
+       
     }
 
     useEffect(()=>{
-        fetchAPis();
-    },[page])
+        fetchApis();
+    },[page]);
 
-
-    const handleScroll =()=>{
+    const handleScroll=useCallback(()=>{
         const Top = document.documentElement.scrollTop;
         const Height = document.documentElement.scrollHeight;
-        const innerHeight = window.innerHeight;
-        if(Top+innerHeight+100>=Height && !loading && hasMore){
+        const innerheight = window.innerHeight;
+        if(Top + innerheight + 100 >= Height && !loading && hasMore){
             setPage((prev)=>prev+1)
+
         }
+        
        
-    }
+    },[loading,hasMore])
 
     useEffect(()=>{
         window.addEventListener("scroll", handleScroll);
         return()=>{
-            window.addEventListener("scroll", handleScroll)
+            window.removeEventListener("scroll", handleScroll)
         }
-    },[])
+    },[handleScroll])
     return(
         <div>
-         <div>
-           {error && <div>Error:{error}</div>}
-            </div>
+        { error &&(
+            <div>Error:{error}</div>
+        ) }
+       {
         
-        {
-            
         products.map((product)=>(
-            <div>
-           
+            <div key={product.id}>
             <img src={product.thumbnail} alt="image"/>
-            
-             </div>
+            </div>
         ))
        
+       }
+        {
+            loading && (
+                <div>Loading....</div>
+            )
         }
-        <div> {loading&&<div>Loading...</div>}</div>
-        
         </div>
     )
 }
