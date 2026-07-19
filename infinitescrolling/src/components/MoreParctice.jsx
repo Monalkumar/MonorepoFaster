631d7ff9e2a7a575e56b95d506333a6c9ff3f42a
MonalKumar <monalshiv123@gmail.com>
2026-07-20 00:09:37 +0530

    APi feth successfully

--- infinitescrolling/src/components/MoreParctice.jsx
+++ infinitescrolling/src/components/MoreParctice.jsx
@@ -1,4 +1,21 @@
+import React, { useState,useEffect } from "react"
+
 const MoreParctice =()=>{
+    const[products, setProducts] = useState([]);
+    const[loading,setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const[hasMore, setHasMore] = useState(true);
+    const[page,setPage] = useState(0)
+
+    const fetchApis =async()=>{
+        const response = await fetch("https://dummyjson.com/products");
+        const result = await response.json();
+        setProducts(result.products);
+        console.log(result.products)
+    }
+    useEffect(()=>{
+        fetchApis()
+    },[])
     return(
         <div>Infinite Scrolling</div>
     )
