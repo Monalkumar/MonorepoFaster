bf0db69f1f86e10d3888bdcb25885b2c8ab7525b
MonalKumar <monalshiv123@gmail.com>
2026-07-21 23:22:20 +0530

    infinite scrolling practice

--- infinitescrolling/src/components/MoreParctice.jsx
+++ infinitescrolling/src/components/MoreParctice.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react"
+import React, { useState,useEffect, useCallback } from "react"
 
 const MoreParctice =()=>{
     const[products, setProducts] = useState([]);
@@ -8,16 +8,66 @@ const MoreParctice =()=>{
     const[page,setPage] = useState(0)
 
     const fetchApis =async()=>{
-        const response = await fetch("https://dummyjson.com/products");
+        try{
+            setLoading(true);
+            setError(null)
+                 const LIMIT = 10;
+        const skip = page*LIMIT
+        const response = await fetch(`https://dummyjson.com/products?limit=${LIMIT}&skip=${skip}`);
         const result = await response.json();
-        setProducts(result.products);
+        setProducts((prev)=>[...prev, ...result.products]);
+        if(result.products.length===0) return setHasMore(false);
+
         console.log(result.products)
+        }
+        catch(error){
+            setError(`error:${error.message}`)
+        }
+        finally{
+            setLoading(false)
+        }
+        
     }
     useEffect(()=>{
         fetchApis()
-    },[])
+    },[page])
+
+    const handleScroll = useCallback(()=>{
+        const Top = document.documentElement.scrollTop;
+        const Height = document.documentElement.scrollHeight;
+        const innerHeight = window.innerHeight;
+        if(Top+innerHeight+100>=Height){
+            setPage((prev)=>prev+1)
+        }
+    },[hasMore,loading])
+
+    useEffect(()=>{
+        window.addEventListener("scroll", handleScroll)
+        return()=>{
+            window.removeEventListener("scroll", handleScroll)
+        }
+    },[handleScroll])
     return(
-        <div>Infinite Scrolling</div>
+        <div>
+        {error && (
+            <div>Error:{error}</div>
+        )}
+        
+        <h1>Infinite Scrolling</h1>
+        {
+           products.length>0 && products.map((product)=>(
+            <div>
+            <img src={product.thumbnail} alt ="product_Image"/>
+            </div>
+           ))
+        }
+        {
+            loading && (
+                <div>Loading.....</div>
+            ) 
+        }
+        
+        </div>
     )
 }
 
